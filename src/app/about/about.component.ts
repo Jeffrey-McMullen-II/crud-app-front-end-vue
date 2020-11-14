de437ad184b49e7835f08d1b4694ab9e7167abc1
Jeffrey <jeffrey.mcmullen@yahoo.com>
2020-11-14 11:03:22 -0500

    WIP

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Vue } from 'vue-class-component';
 import { namespace } from 'vuex-class';
+import axios from 'axios';
 
 import User from './shared/models/User';
 
@@ -9,11 +10,32 @@ export default class About extends Vue {
   @AboutModule.Getter('getUsers') getUsers!: User[];
   @AboutModule.Action('fetchAllUsers') fetchAllUsers!: () => void;
 
+  selectedFile!: File;
+
   get users(): User[] {
     return this.getUsers;
   }
 
   mounted() {
     this.fetchAllUsers();
+  }
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
+    
+  onUploadCLicked() {
+    if (!this.selectedFile) { return; }
+
+    const formData = new FormData();
+    formData.append('name', this.selectedFile.name);
+    formData.append('type', this.selectedFile.type);
+    formData.append('file', this.selectedFile);
+
+    axios.post(
+      'http://localhost/ng-crud-app-backend-php/public/api/files',
+      formData,
+      { headers: {'Content-Type': 'multipart/form-data'}}
+    );
   }
 }
