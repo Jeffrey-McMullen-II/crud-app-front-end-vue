c2cdd278e01688fa6ef6c49ae291563947430c1b
Jeffrey <jeffrey.mcmullen@yahoo.com>
2021-01-10 16:31:28 -0500

    Add paginator to components

--- src/app/prime-vue-components.ts
+++ src/app/prime-vue-components.ts
@@ -2,8 +2,10 @@ import { Component } from 'vue';
 
 import Button from 'primevue/button';
 import InputText from 'primevue/inputtext';
+import Paginator from 'primevue/paginator';
 
 export const primeVueComponents = new Map<string, Component>([
     ['p-button', Button],
-    ['p-inputText', InputText]
+    ['p-inputText', InputText],
+    ['p-paginator', Paginator]
 ]);
