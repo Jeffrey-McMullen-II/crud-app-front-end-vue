78c99f69936aa2dea10a4dca344d63d5db51edcf
Jeffrey <jeffrey.mcmullen@yahoo.com>
2021-01-16 20:55:22 -0500

    Create interfaces for pagination requests and responses

---
+++ src/app/core/shared/models/IPaginationRequest.ts
@@ -0,0 +1,4 @@
+export default interface IPaginationRequest {
+    pageNumber: number | null,
+    resultsPerPage: number | null
+}
---
+++ src/app/core/shared/models/IPaginationResponse.ts
@@ -0,0 +1,4 @@
+export default interface IPaginationRequest {
+    pageCount: number | null,
+    results: any | null
+}
